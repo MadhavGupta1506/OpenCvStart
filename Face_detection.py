5cecc02a56a1a854609acba3ca588e2804e1c29e
MadhavGupta1506 <gmadhav857@gmail.com>
2024-07-17 15:57:19 +0530

    Face detection

--- Face_detection.py
+++ Face_detection.py
@@ -4,6 +4,23 @@ import numpy as np
 img=cv.imread("images/lady.jpg")
 cv.imshow("Image",img)
 
+img=cv.imread("images/lady.jpg")
+cv.imshow("Image",img)
+
+
+img=cv.imread("images/group 1.jpg")
+# cv.imshow("Image",img)
+
+gray=cv.imread("images/group 1.jpg",0)
+cv.imshow("Grqy",gray)
+haar_cascade=cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+faces_rect=haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=1)
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),thickness=2)
+
+cv.imshow("Image",img)
 
 
 cv.waitKey(0)
